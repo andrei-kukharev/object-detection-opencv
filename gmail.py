085dc3b0106da59a8fb108ee42961e059a42d821
Andrei <z@sp.sp>
2020-06-15 14:06:11 +0300

    add gmail

---
+++ gmail.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+
+def send_email():
+
+    port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+    sender_email = "spyphy1@gmail.com"
+    #receiver_email = "spyphy.f@gmail.com"
+    receiver_email = "kukharev.av@mail.ru"
+    password = "sp553866"
+    message = """\
+    Subject: room camera
+
+    something was detected."""
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
